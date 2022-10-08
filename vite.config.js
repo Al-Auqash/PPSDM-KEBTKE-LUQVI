900b0c5c243c5f44bb8e7404a33272447d8e5bf1
Al-Auqash <64571752+Al-Auqash@users.noreply.github.com>
2022-10-08 21:07:44 +0700

    deploy 3

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
+import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
+            input: ["resources/sass/app.scss", "resources/js/app.js"],
             refresh: true,
         }),
         react(),
+        mkcert(),
     ],
+    server: { https: true },
 });
